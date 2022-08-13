8932272a6283016d18b0f34287f7f7c99ca7bb83
bobisnotdead <boristpetrus@sogetrel.fr>
2022-08-13 22:36:41 -0100

    rebase

--- Home.py
+++ Home.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import leafmap.foliumap as leafmap
+import explore
 
 st.set_page_config(layout="wide")
 
--- explore_db.py
+++ explore_db.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 from os import listdir, path
 import sqlite3
 
-c6_database_folder = "/home/acid/Documents/c6_to_send"
+c6_database_folder = "/home/acid/martillac/BEinfo/C6/"
 sqlite_db ='/home/acid/Documents/sqlitedb/sqlit_db'
 conn = sqlite3.connect(sqlite_db) 
 c = conn.cursor()
@@ -14,28 +14,31 @@ c = conn.cursor()
 
 # c6_test_db = path.join(c6_database_folder, 'PM_1ART 1486294_Vouillé_86Z_C6.p')
 # c6_xlsx = path.join(c6_database_folder,'PM_1ART 1486294_Vouillé_86Z_C6.xlsx')
-
-def get_nro():
-    nro = {'nro'}
-    for file in listdir(c6_database_folder):
-        nro.add(file.split('_')[0])
-        # print(file)
-    return nro
+class Nro:     
+    def get_nro():
+        nro = {'nro'}
+        for file in listdir(c6_database_folder):
+            nro.add(file.split('_')[0])
+            # print(file)
+        return nro
 
 
 def pandas_exctract(c6):
-    df = pd.read_excel(c6, 'Export 1', skiprows=7, header =[0])  
-    df = df[['N° appui','Latitude\n(WGS84)','Longitude\n(WGS84)','Adresse de l\'appui (N°, rue ou lieu dit)'] ] 
-    # appui = df['N° appui'].dropna() 
-    return df[df['N° appui'].notna()] 
+    try:
+        df = pd.read_excel(c6, 'Export 1', skiprows=7, header =[0])  
+        df = df[['N° appui','Latitude\n(WGS84)','Longitude\n(WGS84)','Adresse de l\'appui (N°, rue ou lieu dit)'] ] 
+        # appui = df['N° appui'].dropna() 
+        return df[df['N° appui'].notna()] 
+    except Exception as e: 
+        print(e,c6)
 
 def read_all(folder,nro):
     dicdf = {} 
     for file in listdir(folder):
         # if file.endswith('.xlsx') :
-        if file.endswith('.xlsx') and file.startswith(nro):
+        if file.endswith('.xlsx') and file[2:].startswith(nro):
             df = pandas_exctract(path.join(folder, file)) 
-            dicdf[file.split('_')[2]] = df 
+            dicdf[''.join(file.split('_')[2:])[:-5]] = df 
             # cmd = 'CREATE TABLE IF NOT EXISTS BVR (N° appui,Latitude\n(WGS84),Longitude\n(WGS84),Adresse de l\'appui (N°, rue ou lieu dit))' 
             # c.execute(cmd)  
             # conn.commit()
